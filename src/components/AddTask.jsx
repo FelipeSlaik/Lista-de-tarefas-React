91c6b9c812dae0e8085b49f9ebabf6df3b55fe97
FelipeSlaik <felipeslaik@gmail.com>
2025-01-20 09:45:23 -0300

    Criando lista de tarefas e validando campos vazios

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function AddTask({ addNewTask }){
+    const [title, setTitle ]= useState('');
+    const [description, setDescription] = useState('');
+    return(
+        <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+            <input type="text" placeholder="Digite sua tarefa" 
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}/>
+        
+            <input type="text"  placeholder="Digite a descrição da tarefa"
+            className="border border-slate-300 outline-slate-400 px-4 py-2 rounded-md"
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}/>
+
+            <button onClick={() => {
+                if (!title.trim() || !description.trim()){
+                    return alert('Preencha o campo de descrição e titulo');
+                }
+                addNewTask(title, description);
+                setTitle('');
+                setDescription('');
+            }} 
+            className="bg-slate-500 text-white px-4 py-2 rounded-md">
+                Adicionar
+            </button>
+        </div>
+    )
+}
+
+export default AddTask;
